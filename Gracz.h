d3213bbc52185561cbfd833bdada1cca66c2fdad
lewbrajan7-dotcom <lewbrajan7@gmail.com>
2026-07-16 22:41:45 +0200

    Add files via upload

---
+++ Gracz.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include "Jednostka.h"
+#include <vector>
+#include "Przedmiot.h"
+using namespace std;
+class Gracz : public Jednostka{
+private:
+    int Doswiadczenie;
+    int Poziom;
+    int Portfel;
+    int mikstura;
+    vector<Przedmiot> Ekwipunek;
+    Przedmiot Helm = Przedmiot("Brak", 0, 0);
+    Przedmiot Klata = Przedmiot("Brak", 0, 0);
+    Przedmiot Spodnie = Przedmiot("Brak", 0, 0);
+    Przedmiot Buty = Przedmiot("Brak", 0, 0);
+    Przedmiot Miecz = Przedmiot("Brak", 0, 0);
+public:
+    Gracz(string NazwaGracza);
+    void ZyskajDoswiadczenie(int ilosc);
+    void Awansuj();
+    void PokazStatus() override;
+    void DodajPrzedmiot(Przedmiot p);
+    void PokazEkwipunek();
+    void ZalozPrzedmiot();
+    void Sklep();
+    void ZyskajZloto(int ilosc);
+    void UzyjMikstury();
+    void ZapiszGre(int poziomGry);
+    int WczytajGre();
+    bool ProbaUcieczki();
+
+
+} ;
